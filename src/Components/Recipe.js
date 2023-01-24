f4ec26fc55547487cfea9b9fc647aeaaeeb724bd
rohit <rohitdr098@gmail.com>
2023-01-24 23:18:39 +0530

    created recipe card 2

---
+++ src/Components/Recipe.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import RecipeItem from './RecipeItem'
+
+export default function Recipe() {
+  return (
+    <>
+   <div class="container">
+  <div class="row">
+    <div class="col-md-6 mt-4">
+     
+    <RecipeItem></RecipeItem>
+    </div>
+    <div class="col-md-6 mt-4">
+     
+    <RecipeItem></RecipeItem>
+    </div><div class="col-md-6 mt-4">
+     
+     <RecipeItem></RecipeItem>
+     </div><div class="col-md-6 mt-4">
+     
+     <RecipeItem></RecipeItem>
+     </div><div class="col-md-6 mt-4">
+     
+     <RecipeItem></RecipeItem>
+     </div><div class="col-md-6 mt-4">
+     
+     <RecipeItem></RecipeItem>
+     </div>
+  </div>
+</div>
+        
+<div class="d-flex justify-content-between">
+        <button type="button" class="btn btn-dark" disabled={this.state.page<=1} onClick={this.prev}>previous</button>
+        <button type="button" class="btn btn-dark" disabled={this.state.page+1>Math.ceil(this.state.total/20)} onClick={this.next}>Next</button>
+        </div>  
+
+    
+
+   
+
+    
+    </>
+  )
+}
---
+++ src/Components/RecipeItem.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import Button from './Button'
+
+export default function RecipeItem() {
+  return (
+   <>
+   <div className="card">
+   <div class="d-flex bd-highlight">
+  <div class="p-2 flex-fill bd-highlight" style={{width: "50%"}}>
+  <img height="300px" width="500px"src="https://edamam-product-images.s3.amazonaws.com/web-img/4c3/4c3f3534d8ac2c9849a7c78767dcbc29.jpg?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEIf%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJIMEYCIQCbRbqqt1IaEj6X%2FoJQta%2BzET%2F6QhtEl5iCBtTcA%2FXCCwIhAMgMm%2F5fSNqebecDYVihDaLN0SZfmjr1XND%2Bbjwr3WR0KtUECPD%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMMTg3MDE3MTUwOTg2IgwvW1ZnVF6hn%2FmzMuUqqQSQuJ1ie2Rhq7r%2Bh4BisBGpG0GV3Mer2ZPZmWNGkbFA9ANeWY26t8ieoPDw9ZVg8Ye5ZhfEaflv8y0%2FbvD2GEYSsbI2VnGegYCE09WpeIgnoMo61SfuEyqSj%2BdNnkYs8S4gpr18SvnuOFsLRdhuxnQXT723622NQdK%2B%2B2M4ctfkWgdNM1ReWXnXQ28GZeA9VYlit%2BYZ2kxpJhO5STglA9wspvSyTYlM4a6%2FpDeuaGPpYgAqZerC55lxeaEsF8gqs3ZNBgkbRMqN%2FE3D1lrU3smjyT0yLbd0j77sxdUfiNxVBchbB3u%2BMNs6Ci9tCYQ1RCktDVb5QY5GNdLYbteb0yeqmyYx3pgxdRod8TNGO2AM8aVLVyUzJQmq1s41GyO%2F4ggA3Fj0ldIChuZz8jc%2B0RbQXk5dbWUcX0Vl8xGOn2jNRFBV%2FwzNbLBInmgSKSMbPsUFt9yeCzGziZz8yIxjUw%2BjXH1TrD2PcmYy16iy0R7yb%2B3wnYZ9CISBJ56ZIEECKb%2FOOeWrQ6tT5vI%2FKKPg7s2tjt5GhhDxOowYNHS6eZ1KikITvblfo1%2BxRQxNPla87Ex66ZL0SYdgQxeAo%2Fdvprs746h6lwwjdjpAp28FD3iQUBPl4AIUt4yiqmsFjjZxyFRAylvsK9aR7lOVQiDgWIp4YlI5UI0V4TVCWE5i7JAxRfK8zxY8C2uHbHm6ofFO3p%2BlDd4fkgvMQLllS00rAYKHG2sy7B%2F9%2BfY9MIrZv54GOqgBUzpQky%2FgRuRF5FvEKwpUhneyJaFH%2BxtbAMnGrblTQuhZJb0WeCP1ZP0yOH%2By%2B%2BauhirJ0X821p7jYq76YplUQGCBomj%2FgbzhFfx52t6rE1mMTRrc4mdgFC4GJIwTR8Dd169GI3QyYDpkeH5Uee1XwGh0rYfr5t1GEDZJIVPgv85rpopeXMykkxuoZ1aRxrqpEH1ihQGCFora4xke5sqfbS1y4X6%2F01Y8&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230124T153023Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIASXCYXIIFCLQJJMDE%2F20230124%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=6e2093eeca6d132e2e911f136491fa97e566731444af5fadf4d7a6273ace892c" className="card-img-top" alt="..."/>
+
+  </div>
+  <div class="p-2 flex-fill bd-highlight " style={{width: "50%"}}><h3 class="text-center">Basic Dry Cure</h3>
+    <p className="card-text ">"Low Potassium",
+"Kidney-Friendly",
+"Vegan",
+"Vegetarian",
+"Pescatarian",
+"Dairy-Free",
+"Gluten-Free",
+"Wheat-Free",
+"Egg-Free",
+"Peanut-Free",
+"Tree-Nut-Free",
+"Soy-Free",
+"Fish-Free",
+"Shellfish-Free",
+"Pork-Free",
+"Red-Meat-Free",
+</p>
+   </div>
+
+</div> 
+<hr class="hr" />
+  <div className="card-body " style={{backgroundColor:"rgb(63 52 81 / 15%)"}}>
+  <div class="d-flex bd-highlight">
+  <div class="p-2 flex-fill bd-highlight"  style={{width: "50%"}}>
+
+    
+    <h3 class="text-center">25 servers</h3>
+    <h1 class="text-center">235 kcal</h1>
+  <table class=" table table-borderless table-striped table-hover my-4">
+ 
+  <tbody>
+    <tr>
+
+      <td>Mark</td>
+      <td>Otto</td>
+  
+    </tr>
+    <tr>
+     
+      <td>Jacob</td>
+      <td>Thornton</td>
+    
+    </tr>
+    <tr>
+    <td>Jacob</td>
+      <td>Thornton</td>
+     
+    </tr>
+  </tbody>
+</table>
+<Button type="primary" label="Read More"></Button>
+  </div>
+  <div class="p-2 flex-fill bd-highlight"><h5 className="card-title"></h5>
+  <table class=" table table-borderless table-striped table-hover">
+ 
+ <tbody>
+   <tr>
+
+     <td>Mark</td>
+     <td>Otto</td>
+     
+   </tr>
+   <tr>
+    
+     <td>Jacob</td>
+     <td>Thornton</td>
+    
+   </tr>
+   <tr>
+   <td>Jacob</td>
+     <td>Thornton</td>
+   
+   </tr>
+   <tr>
+   <td>Jacob</td>
+     <td>Thornton</td>
+   
+   </tr>
+   <tr>
+   <td>Jacob</td>
+     <td>Thornton</td>
+   
+   </tr>
+   <tr>
+   <td>Jacob</td>
+     <td>Thornton</td>
+   
+   </tr>
+   <tr>
+   <td>Jacob</td>
+     <td>Thornton</td>
+   
+   </tr>
+ </tbody>
+</table>
+</div> 
+</div> 
+</div> 
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+   
+   
+   </>
+  )
+}
