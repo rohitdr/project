946d907269dd83e068ddc191322fb5e231bbdbbf
rohit <rohitdr098@gmail.com>
2023-01-26 23:54:08 +0530

    added colors to body

---
+++ src/Components/Individual_Recipe.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class Individual_Recipe extends Component {
+  render() {
+    return (
+      <>
+     
+      
+      
+      
+     <div class="container mt-2">
+  <div class="row">
+    <div class="col-sm-5 col-md-6"><img  height="300px" width="500px" src="https://www.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg" alt="" srcset="" /></div>
+         <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
+
+         </div>
+             </div>
+  <div class="row">
+    <div class="col-sm-6 col-md-5 col-lg-6">.col-sm-6 .col-md-5 .col-lg-6 Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore reprehenderit ipsam, odio atque possimus laborum recusandae expedita harum. Amet doloribus, similique ullam quas, deleniti provident hic optio ratione eos fugiat nobis excepturi ab praesentium quae quisquam corporis aliquid ut, ex facere maxime corrupti odio distinctio rerum! Eveniet necessitatibus nisi esse.</div>
+    <div class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">.col-sm-6 .col-md-5 .offset-md-2 .col-lg-6 .offset-lg-0 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, pariatur cum quibusdam vitae minima quaerat dolor, voluptate delectus consequatur ratione perferendis iusto aliquam unde mollitia in quae rem, ea autem. Nam, animi accusantium non eveniet magnam sint sapiente facere hic ea porro laboriosam id maxime repellendus nihil recusandae sunt inventore?</div>
+  </div>
+</div>
+      
+      
+      
+      
+      </>
+    )
+  }
+}
