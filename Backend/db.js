3db59a1cb54b1a64b88a46362940ffe11763a98a
rohit <rohitdr098@gmail.com>
2023-02-03 13:33:33 +0530

    connected to mongo db

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose')
 const Mongoose_uri ="mongodb://localhost:27017"
 const mongooseConnectToDb=()=>{
 mongoose.connect((Mongoose_uri), ()=>{
-    console.log("connected to mongo db")
+    console.log("connected to mongo db");
 })
 }
-module.export= mongooseConnectToDb
+module.exports= mongooseConnectToDb;
--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,14 @@
-const mongooseConnectToDb=require('./db.js')
+const mongooseConnectToDb=require('./db')
 const express = require('express')
-mongooseConnectToDb();
+
 const app = express()
 const port = 500
-
+mongooseConnectToDb();
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
-app.use('./api/auth',require('./Routes/auth.js'))
-app.use('./api/recipe',require('./Routes/recipe.js'))
+// app.use('./api/auth',require('./Routes/auth.js'))
+// app.use('./api/recipe',require('./Routes/recipe.js'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
