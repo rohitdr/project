4cb5b0c19f8b993049389b8c40106dc9e1d56a50
rohit <rohitdr098@gmail.com>
2024-02-10 21:22:52 +0530

    some

--- Backend/Config/Dev.js
+++ Backend/Config/Dev.js
@@ -3,6 +3,7 @@ module.exports={
     jwtSecret:"adlksfjakghaslkdfj",
     Mongoose_uri:"mongodb+srv://Rohitdr098:Rohit%402001@cluster0.sne14ef.mongodb.net/Recipe",
     // Mongoose_uri:"mongodb+srv://rohitdr098:Rohit@2001@cluster0.fcgekeu.mongodb.net/Recipe",
+   
 
     AdminEmail:"rohitdr098@gmail.com"
 
